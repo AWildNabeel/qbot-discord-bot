6bd61938aabc87ac54cf77a98f91fb32862d25e4
Lengo <28582329+yogurtsyum@users.noreply.github.com>
2021-06-06 19:11:20 -0500

    Fix bot status bug

--- src/index.js
+++ src/index.js
@@ -208,7 +208,7 @@ client.on('ready', async () => {
     console.log(`${chalk.hex('#60bf85')('Bot started!')}\n⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯\n`
     + `${chalk.hex('#ffaa2b')('>')} ${chalk.hex('#7289DA')(`Servers: ${chalk.hex('#4e5f99')(`${client.guilds.cache.size}`)}`)}\n`
     + `${chalk.hex('#ffaa2b')('>')} ${chalk.hex('#7289DA')(`Channels: ${chalk.hex('#4e5f99')(`${client.channels.cache.size}`)}`)}`);
-    let botstatus = fs.readFileSync('./src/bot-status.json');
+    let botstatus = fs.readFileSync('./bot-status.json');
     botstatus = JSON.parse(botstatus);
     if(botstatus.activity == 'false') return;
     if(botstatus.activitytype == 'STREAMING'){
